3d0d92333559d56e9b7bad930b44adda59b218b7
meslammo <meslammohamed423@gmail.com>
2026-08-21 03:19:27 +0300

    Fix Flutter baseline configuration and model serialization

--- lib/models/item_model.dart
+++ lib/models/item_model.dart
@@ -1,10 +1,8 @@
-import 'package:json_annotation/json_annotation.dart';
-
-part 'item_model.g.dart';
+import 'package:flutter/foundation.dart';
 
 enum ItemRarity { common, rare, epic, legendary }
 
-@JsonSerializable()
+@immutable
 class ItemModel {
   final String id;
   final String name;
@@ -13,10 +11,10 @@ class ItemModel {
   final ItemRarity rarity;
   final int basePrice;
   final int quantity;
-  final String type; // 'cosmetic', 'tool', 'resource', 'material'
+  final String type;
   final Map<String, dynamic> properties;
 
-  ItemModel({
+  const ItemModel({
     required this.id,
     required this.name,
     required this.description,
@@ -28,6 +26,33 @@ class ItemModel {
     this.properties = const {},
   });
 
-  factory ItemModel.fromJson(Map<String, dynamic> json) => _$ItemModelFromJson(json);
-  Map<String, dynamic> toJson() => _$ItemModelToJson(this);
+  factory ItemModel.fromJson(Map<String, dynamic> json) {
+    final rarityValue = json['rarity']?.toString();
+    return ItemModel(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      description: json['description'] as String,
+      icon: json['icon'] as String,
+      rarity: ItemRarity.values.firstWhere(
+        (value) => value.name == rarityValue,
+        orElse: () => ItemRarity.common,
+      ),
+      basePrice: (json['basePrice'] as num).toInt(),
+      quantity: (json['quantity'] as num?)?.toInt() ?? 1,
+      type: json['type'] as String,
+      properties: Map<String, dynamic>.from(json['properties'] as Map? ?? const {}),
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'name': name,
+        'description': description,
+        'icon': icon,
+        'rarity': rarity.name,
+        'basePrice': basePrice,
+        'quantity': quantity,
+        'type': type,
+        'properties': properties,
+      };
 }
